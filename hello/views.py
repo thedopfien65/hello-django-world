c94d456e8a27693b3bc4ceef125ef1201892d8e8
Bryan Burnham <thedopfien65@gmail.com>
2017-04-03 02:36:08 +0000

    HW1 Done. Added a 'lucky number' to the hello app.

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from random import *
 
 # Create your views here.
+randNum = randint(1, 100);
+
 def index(request):
-        return HttpResponse("Hello, DJango World!")
+        return HttpResponse("Hello, DJango World! Your lucky number is " + str(randNum))
